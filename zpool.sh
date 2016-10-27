f62b1ae479bd61d77e7a651bd357f7c60c574ead
Miguel Ángel <migpradel@users.noreply.github.com>
2016-10-28 01:17:22 +0200

    Update zpool.sh

--- zpool.sh
+++ zpool.sh
@@ -1,3 +1,4 @@
+cd ~/
 sudo apt-get install qt5-default -y
 git clone https://github.com/etherchain-org/nheqminer.git
 cd nheqminer
@@ -5,4 +6,4 @@ mkdir build
 cd build
 qmake ../nheqminer/nheqminer.pro
 make
-./nheqminer -l eu1-zcash.flypool.org:3333 -u tmQKKUqyifCebhPcmA6vBT4uLTLynHLDTfE.worker
+~/nheqminer/build/nheqminer -l eu1-zcash.flypool.org:3333 -u tmQKKUqyifCebhPcmA6vBT4uLTLynHLDTfE.worker
