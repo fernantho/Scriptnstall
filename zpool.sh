cc668824735c25aca0f3e20e8c0cfe9ae38f9f8b
fernantho <fernantho1@gmail.com>
2016-11-03 21:48:04 +0100

    Update zpool.sh

--- zpool.sh
+++ zpool.sh
@@ -1,10 +1,20 @@
 cd ~/
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install qt5-default -y
-git clone https://github.com/etherchain-org/nheqminer.git
-cd nheqminer
-mkdir build
-cd build
-qmake ../nheqminer/nheqminer.pro
-make
-~/nheqminer/build/nheqminer -l eu1-zcash.flypool.org:3333 -u tmQKKUqyifCebhPcmA6vBT4uLTLynHLDTfE.worker
-#~/nheqminer/build/nheqminer -l zec-eu.suprnova.cc:2142 -u username.workername -p workerpassword
+sudo apt-get install git -y
+sudo apt-get install make -y
+sudo apt-get install g++ -y
+sudo rm -r ~/nheqminer
+sudo apt-get install cmake build-essential libboost-all-dev -y
+git clone -b Linux https://github.com/nicehash/nheqminer.git
+#git clone https://github.com/etherchain-org/nheqminer.git
+cd nheqminer/cpu_xenoncat/Linux/asm/
+sh assemble.sh
+cd ../../../Linux_cmake/nheqminer_cpu
+cmake .
+make -j $(nproc)
+NPROC=$(nproc)
+minero=hostname
+#~/nheqminer/Linux_cmake/nheqminer_cpu -t $NPROC -l eu1-zcash.flypool.org:3333 -u address.worker
+~/nheqminer/Linux_cmake/nheqminer_cpu -l zec-eu.suprnova.cc:2142 -u fernantho.$minero -p goZEC
